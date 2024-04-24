6901d9a8cb34c3912da78c9886f7c287a13ff1b6
MeLlamoGomo <gmorales9600@gmail.com>
2024-04-24 09:35:52 -0700

    dropdownAnimation

--- script.js
+++ script.js
@@ -56,3 +56,31 @@ function myMenu() {
     document.getElementsByClassName('faq_toggle').classList.toggle('faq-open')
   } */
 
+  (function() {
+    var elements;
+    var windowHeight;
+  
+    function init() {
+      elements = document.querySelectorAll('.hidden');
+      windowHeight = window.innerHeight;
+    }
+  
+    function checkPosition() {
+      for (var i = 0; i < elements.length; i++) {
+        var element = elements[i];
+        var positionFromTop = elements[i].getBoundingClientRect().top;
+  
+        if (positionFromTop - windowHeight <= 0) {
+          element.classList.add('pop_in');
+          element.classList.remove('hidden');
+        }
+      }
+    }
+  
+    window.addEventListener('scroll', checkPosition);
+    window.addEventListener('resize', init);
+  
+    init();
+    checkPosition();
+  })();
+
